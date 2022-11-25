1a88a702993779eb1d8260b21f0a9ae4f5a8f6c2
Dhara00719 <Dhara00719@gmail.com>
2022-11-25 14:40:42 +0530

    add world to local vector

--- Assets/Transformation/Transformation.cs
+++ Assets/Transformation/Transformation.cs
@@ -4,14 +4,31 @@ using UnityEngine;
 
 public class Transformation : MonoBehaviour
 {
-    Vector2 WorldPos;
+    [Header("World To Local")]
+    public Vector2 WorldPos;
+    [Header("Result of World To Local (Do not change)")]
+    public Vector2 resultLocalPos;
     [Header("Local To World (Cyan)")]
     public Vector2 LocalPos;
     private void OnDrawGizmos()
     {
+        //Local to world
         Gizmos.color = Color.cyan;
         Vector2 wPos = localToWorld(LocalPos);
         Gizmos.DrawSphere(wPos, 0.1f);
+
+        //world to local
+        Gizmos.color = Color.blue;
+        resultLocalPos = worldToLocal(WorldPos);
+        Gizmos.DrawSphere(WorldPos, 0.1f);
+    }
+
+    public Vector2 worldToLocal(Vector2 world)
+    {
+        Vector2 rel = world - (Vector2)transform.position;
+        float x = Vector2.Dot(rel, transform.right);
+        float y = Vector2.Dot(rel, transform.up);
+        return new(x,y);
     }
 
     public Vector2 localToWorld(Vector2 local)
